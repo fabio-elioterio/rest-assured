c63c0fa82c1667bbcb314e6966a90718a5a76b10
fabio-elioterio <fabioelioterio.77@gmail.com>
2020-09-03 17:50:12 -0300

    Refreshing the code

---
+++ src/test/java/br/ce/fabio/rest/SchemaTest.java
@@ -0,0 +1,50 @@
+package br.ce.fabio.rest;
+
+import io.restassured.matcher.RestAssuredMatchers;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.Test;
+import org.xml.sax.SAXParseException;
+
+import static io.restassured.RestAssured.given;
+
+public class SchemaTest {
+
+    @Test
+    public void deveValidarSchemaXML() {
+        given()
+                .log().all()
+                .when()
+                    .get("http://restapi.wcaquino.me/usersXML")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
+                ;
+    }
+
+    @Test(expected = SAXParseException.class)
+    public void naoDeveValidarSchemaXMLInvalido() {
+        given()
+                .log().all()
+                .when()
+                .get("http://restapi.wcaquino.me/invalidUsersXML")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
+        ;
+    }
+
+    @Test
+    public void deveValidarSchemaJson() {
+        given()
+                .log().all()
+                .when()
+                    .get("http://restapi.wcaquino.me/users")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("users.json"))
+        ;
+    }
+}
