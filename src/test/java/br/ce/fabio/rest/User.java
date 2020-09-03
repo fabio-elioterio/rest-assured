7df8792350c7e30b60f4153fd28d06ce256fec6b
fabio-elioterio <fabioelioterio.77@gmail.com>
2020-09-03 14:38:33 -0300

    using the serialization

---
+++ src/test/java/br/ce/fabio/rest/User.java
@@ -0,0 +1,67 @@
+package br.ce.fabio.rest;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "user") // classe superior
+@XmlAccessorType(XmlAccessType.FIELD) // ele pega todos os atributos e gets que não estejam cobertos.
+public class User {
+
+    private String name;
+    private Integer age;
+    private Double salary;
+    @XmlAttribute
+    private Long id;
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
+}
--- src/test/java/br/ce/fabio/rest/VerbosTests.java
+++ src/test/java/br/ce/fabio/rest/VerbosTests.java
@@ -1,10 +1,15 @@
 package br.ce.fabio.rest;
 
 import io.restassured.http.ContentType;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 public class VerbosTests {
 
@@ -23,6 +28,71 @@ public class VerbosTests {
                     .body("name", is("Jose"))
                     .body("age", is(50))
                 ;
+
+    }
+
+    @Test
+    public void deveSalvarUsuarioMap() {
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("name", "Usuario via map");
+        params.put("age", 25);
+
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body(params)
+                .when()
+                .post("http://restapi.wcaquino.me/users")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("id", is(notNullValue()))
+                .body("name", is("Usuario via map"))
+                .body("age", is(25))
+        ;
+
+    }
+
+    @Test
+    public void deveSalvarUsuarioObjeto() {
+        User user = new User("Usuario via objeto", 35);
+
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body(user)
+                .when()
+                    .post("http://restapi.wcaquino.me/users")
+                .then()
+                    .log().all()
+                    .statusCode(201)
+                    .body("id", is(notNullValue()))
+                    .body("name", is("Usuario via objeto"))
+                    .body("age", is(35))
+        ;
+
+    }
+
+    @Test
+    public void deveDeserializarObjetoAoSalvarUsuario() {
+        User user = new User("Usuario deserializado", 35);
+
+       User usuarioInserido = given()
+                .log().all()
+                .contentType("application/json")
+                .body(user)
+                .when()
+                    .post("http://restapi.wcaquino.me/users")
+                .then()
+                    .log().all()
+                    .statusCode(201)
+                    .extract().body().as(User.class)
+        ;
+
+        System.out.println(usuarioInserido);
+        assertEquals("Usuario deserializado", usuarioInserido.getName());
+        assertThat(usuarioInserido.getAge(), is(35));
+        assertThat(usuarioInserido.getId(), notNullValue());
 
     }
 
@@ -59,5 +129,129 @@ public class VerbosTests {
                 .body("user.age", is("50"                     ))
         ;
 
+    }
+
+    @Test
+    public void deveDeserializarXMLAoSalvarUsuario() {
+        User user = new User("Usuario XML", 40);
+
+        User usuarioInserido = given()
+                .log().all()
+                .contentType(ContentType.XML)
+                .body(user)
+                .when()
+                    .post("http://restapi.wcaquino.me/usersXML")
+                .then()
+                    .log().all()
+                    .statusCode(201)
+                    .extract().body().as(User.class )
+        ;
+        assertThat(usuarioInserido.getId(), is(notNullValue()));
+        assertThat(usuarioInserido.getName(), is("Usuario XML"));
+        assertThat(usuarioInserido.getAge(), is(40));
+        assertThat(usuarioInserido.getSalary(), nullValue());
+    }
+
+    @Test
+    public void deveSalvarUsuarioViaXMLUsandoObjeto() {
+        User user = new User("Usuario XML", 40);
+        given()
+                .log().all()
+                .contentType(ContentType.XML)
+                .body(user)
+                .when()
+                .post("http://restapi.wcaquino.me/usersXML")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("user.@id", is(notNullValue()))
+                .body("user.name", is("Usuario XML"))
+                .body("user.age", is("40"))
+        ;
+
+    }
+
+    @Test
+    public void deveAlterarUsuario() {
+        given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"Usuario alterado\", \"age\": 80 }")
+                .when()
+                    .put("http://restapi.wcaquino.me/users/1")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("id", is(1))
+                    .body("name", is("Usuario alterado"))
+                    .body("age", is(80))
+                    .body("salary", is(notNullValue()))
+        ;
+
+    }
+
+    @Test
+    public void deveCustomizarURL() {
+        given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"Usuario alterado\", \"age\": 80 }")
+                .when()
+                    .put("http://restapi.wcaquino.me/{entidade}/{userId}", "users", "1")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("id", is(1))
+                    .body("name", is("Usuario alterado"))
+                    .body("age", is(80))
+                    .body("salary", is(notNullValue()))
+        ;
+
+    }
+
+    @Test
+    public void deveCustomizarURLParte2() {
+        given()
+                .log().all()
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"Usuario alterado\", \"age\": 80 }")
+                .pathParam("entidade", "users")
+                .pathParam("userId", "1")
+                .when()
+                    .put("http://restapi.wcaquino.me/{entidade}/{userId}")
+                .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("id", is(1))
+                    .body("name", is("Usuario alterado"))
+                    .body("age", is(80))
+                    .body("salary", is(notNullValue()))
+        ;
+
+    }
+
+    @Test
+    public void deveRemoverUsuario() {
+        given()
+                .log().all()
+                .when()
+                    .delete("http://restapi.wcaquino.me/users/1")
+                .then()
+                    .log().all()
+                    .statusCode(204)
+                ;
+    }
+
+    @Test
+    public void naoDeveRemoverUsuarioInexistente() {
+        given()
+                .log().all()
+                .when()
+                    .delete("http://restapi.wcaquino.me/users/1000")
+                .then()
+                    .log().all()
+                    .statusCode(400)
+                    .body("error", is("Registro inexistente"))
+        ;
     }
 }
