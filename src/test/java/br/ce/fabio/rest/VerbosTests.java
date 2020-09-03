9bda8a7c73b9d698f05e89ce21e64e9276a29656
fabio-elioterio <fabioelioterio.77@gmail.com>
2020-09-03 11:53:09 -0300

    working with rest verbs

--- src/test/java/br/ce/fabio/rest/VerbosTests.java
+++ src/test/java/br/ce/fabio/rest/VerbosTests.java
@@ -1,2 +1,63 @@
-package br.ce.fabio.rest;public class VerbosTests {
+package br.ce.fabio.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class VerbosTests {
+
+    @Test
+    public void deveSalvarUsuario() {
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body("{\"name\": \"Jose\", \"age\": 50 }")
+                .when()
+                    .post("http://restapi.wcaquino.me/users")
+                .then()
+                    .log().all()
+                    .statusCode(201)
+                    .body("id", is(notNullValue()))
+                    .body("name", is("Jose"))
+                    .body("age", is(50))
+                ;
+
+    }
+
+    @Test
+    public void naoDeveSalvarUsuarioSemNome() {
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body("{\"age\": 50 }")
+                .when()
+                .post("http://restapi.wcaquino.me/users")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("id", is(nullValue()))
+                .body("error", is("Name é um atributo obrigatório"))
+        ;
+
+    }
+
+    @Test
+    public void deveSalvarUsuarioViaXML() {
+        given()
+                .log().all()
+                .contentType(ContentType.XML)
+                .body("<user><name>Jose</name><age>50</age></user>")
+                .when()
+                    .post("http://restapi.wcaquino.me/usersXML")
+                .then()
+                    .log().all()
+                    .statusCode(201)
+                .body("user.@id", is(notNullValue()))
+                .body("user.name", is("Jose"))
+                .body("user.age", is("50"                     ))
+        ;
+
+    }
 }
